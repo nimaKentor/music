1a9004901ed8c6d29bcf18f77fff5fadc14bfa43
nimaKentor <nima.mazdak@soprasteria.com>
2019-01-23 17:26:55 +0100

    minor changes

--- src/main/java/com/eurobaluchi/music/rest/controller/BaseController.java
+++ src/main/java/com/eurobaluchi/music/rest/controller/BaseController.java
@@ -3,4 +3,5 @@ package com.eurobaluchi.music.rest.controller;
 import com.eurobaluchi.music.rest.service.BaseServiceApi;
 
 public abstract class BaseController<T, N, S extends BaseServiceApi<T, N>> {
+    //Remove this
 }
